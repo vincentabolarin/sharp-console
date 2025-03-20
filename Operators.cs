d8997c4637b8857448232d09793b82333672a3b3
Vincent Abolarin <80973232+vincentabolarin@users.noreply.github.com>
2025-03-20 21:59:51 +0100

    implement operators

---
+++ Operators.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Operators
+{
+	public static void All()
+	{
+        Console.WriteLine("OPERATORS");
+		Addition();
+	}
+	private static void Addition()
+	{
+		int a = 6;
+		int b = 2;
+
+		int c = a + b;
+        Console.WriteLine(c);
+	}
+}

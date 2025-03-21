00f194df4cb3db192cc01a3f38272c4d043ece20
Vincent Abolarin <80973232+vincentabolarin@users.noreply.github.com>
2025-03-21 07:49:41 +0100

    add operators

--- Operators.cs
+++ Operators.cs
@@ -2,17 +2,49 @@
 
 public class Operators
 {
+
+	private static double a = 8;
+	private static int b = 3;
 	public static void All()
 	{
         Console.WriteLine("OPERATORS");
+		Increment();
+		Decrement();
 		Addition();
+		Subtraction();
+		Multiplication();
+		Division();
+		Remainder();
+	}
+
+	private static void Increment()
+	{
+		a++;
+        Console.WriteLine(a);
+	}
+	private static void Decrement()
+	{
+		b -= 1;
+		Console.WriteLine(b);
 	}
 	private static void Addition()
 	{
-		int a = 6;
-		int b = 2;
-
-		int c = a + b;
-        Console.WriteLine(c);
+        Console.WriteLine(a + b);
+	}
+	private static void Subtraction()
+	{
+		Console.WriteLine(a - b);
+	}
+	private static void Multiplication()
+	{
+		Console.WriteLine(a * b);
+	}
+	private static void Division()
+	{
+		Console.WriteLine(a / b);
+	}
+	private static void Remainder()
+	{
+		Console.WriteLine(a % b);
 	}
 }
